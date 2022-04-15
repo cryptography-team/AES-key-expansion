2d402d7bbe60c5cad646e8754fa365e5ac10a745
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-04-15 21:31:40 +0200

    input can now be taken continuously

--- main.cpp
+++ main.cpp
@@ -5,18 +5,27 @@ using namespace AES;
 using namespace std;
 int main() {
   keyExpansion tool;
-  word input;
-  word expandKEy[44];
+  word expandKey[44];
   byte key[16];
-  cout << "Enter your key \n";
-  for (word i = 0; i < 16; i++) {
+  cout << "Enter your key:" << endl;
+  // Hold the values in a proper form in a stringstream
+  stringstream ss;
+  for (int i = 0; i < 32; i++) {
+    char c;
+    cin >> c;
+    if (i % 2 == 0)
+      ss << ' ';
+    ss << c;
+  }
+  for (int i = 0; i < 16; i++) {
+    word input;
     // To get hex inputs, use the built-in stl function hex.
-    cin >> hex >> input;
+    ss >> hex >> input;
     key[i] = input;
   }
-  tool.expand(key, expandKEy);
+  tool.expand(key, expandKey);
   cout << "\nThe expanded key is: \n";
-  for (unsigned int i = 0, round = 1, cont = 4; i < 44; ++cont, i++) {
+  for (int i = 0, round = 1, cont = 4; i < 44; ++cont, i++) {
     if (cont == 4) {
       cout << dec << endl
            << "Round " << setfill(' ') << setw(2) << round << ": ";
@@ -25,7 +34,7 @@ int main() {
     }
     // fill empty places with '0'
     cout << setfill('0') << setw(8);
-    cout << hex << expandKEy[i] << " ";
+    cout << hex << expandKey[i] << " ";
   }
   return 0;
 }
