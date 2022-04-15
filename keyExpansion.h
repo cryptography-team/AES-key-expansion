8bd45d4de51e80c387a12768e7e9a75da8a9ab6e
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-04-15 23:41:43 +0200

    major change in the structure idea

--- keyExpansion.h
+++ keyExpansion.h
@@ -1,21 +1,22 @@
 #ifndef KEY_EXPANSION_HEADER
 #define KEY_EXPANSION_HEADER
+#include <iostream>
+using namespace std;
 typedef unsigned char byte;
 typedef unsigned int word;
 namespace AES {
 
 class keyExpansion {
 protected:
-  byte RC[10], transformationMatrix[8], xorColumn;
+  byte RC[10], substitutionTable[16][16];
 
 public:
-  keyExpansion();                              // yasser
-  void init(byte key[16], word w[4]) const;    // yasser
-  byte transformByte(const byte &b) const;     // muns
-  byte subByte(const byte &b) const;           // muns
-  word subWord(const word &w) const;           // muns
-  word rotWord(const word &w) const;           // yasser
-  void expand(byte key[16], word w[44]) const; // muns
+  keyExpansion();
+  void initExpand(byte key[16], word w[4]) const;
+  void initTable();
+  word subWord(const word &w) const;
+  word rotWord(const word &w) const;
+  void expand(byte key[16], word w[44]) const;
 };
 
 } // namespace AES
