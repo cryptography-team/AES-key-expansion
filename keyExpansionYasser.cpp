123e647ed2ed5c2bef4070d62af88f993d3ec597
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-04-15 23:42:20 +0200

    removed old unused function definitions

--- keyExpansionYasser.cpp
+++ keyExpansionYasser.cpp
@@ -3,8 +3,7 @@
 using AES::keyExpansion;
 
 // This function initializes the constants as described in the reference
-// xorColumn is a vector used to get the substitution for any byte by XOR
-keyExpansion::keyExpansion() : xorColumn(0x63) {
+keyExpansion::keyExpansion() {
   // RC is used to obtain Rcon
   RC[0] = 0x01;
   for (int i = 1; i < 8; ++i)
@@ -12,18 +11,11 @@ keyExpansion::keyExpansion() : xorColumn(0x63) {
   RC[8] = 0x1B;
   RC[9] = 0x36;
 
-  // This is a matrix used to get the substitution for any byte by matrix
-  // multiplication
-  transformationMatrix[0] = 0xF1;
-  for (int i = 1; i < 8; ++i)
-    // Every row (except the first one) is the same as the the previous row but
-    // after applying a left cyclic bit shift
-    transformationMatrix[i] =
-        (transformationMatrix[i - 1] << 1) | (transformationMatrix[i - 1] >> 7);
+  initTable();
 }
 
 // This function puts the given key into the first four words
-void keyExpansion::init(byte key[16], word w[4]) const {
+void keyExpansion::initExpand(byte key[16], word w[4]) const {
   for (int i = 0; i < 4; ++i)
     for (int j = 0; j < 4; ++j) {
       // Left byte shift
@@ -34,4 +26,8 @@ void keyExpansion::init(byte key[16], word w[4]) const {
 
 // This function makes a left cyclic byte shift by discarding the last byte and
 // ORing it at the beginning (32 - 8 = 24)
-word keyExpansion::rotWord(const word &w) const { return (w << 8) | (w >> 24); }
+word keyExpansion::rotWord(const word &w) const {
+  word tmp = (w << 8) | (w >> 24);
+  cout << "rotWord = " << hex << tmp << endl;
+  return tmp;
+}
