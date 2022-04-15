a5cd5cd16648e9ec0fb6dea0c8d5fd72311683ae
mohamed Mansour <67611571+muns99@users.noreply.github.com>
2022-04-15 04:07:14 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
+#include <iomanip>
+#include <iostream>
 #include "keyExpansion.h"
 using namespace AES;
 using std::cin;
 using std::hex;
+using std::dec;
 using std::cout;
 using std::endl;
+using std::setw;
 int main()
 {
-    word x;
+    keyExpansion tool ;
+    word input;
     word expandKEy[44];
     byte key[16] ;
     cout<<"Enter your key \n";
     for(word i = 0 ; i<16;i++)
     {
-        cin >> hex >> x;
-        key[i] = x;
+        // To get hex inputs, use the built-in stl function hex.
+        cin >> hex >> input;
+        key[i] = input;
     }
-    keyExpansion tool ;
     tool.expand(key,expandKEy);
-    cout<<"\n Expansion key is \n";
-    for(word i = 0,cont=1;i<44;++cont,i++)
+    cout<<"\nThe expanded key is: \n";
+    for(unsigned int i = 0,round = 1,cont=4;i<44;++cont,i++)
     {
-        cout<<expandKEy[i]<<" ";
         if(cont == 4)
         {
-            cout<<endl;
+            cout<<dec<<endl<<"Round "<<std::setfill('0')<<setw(2)<<round<<": ";
+            round++;
             cont = 0;
         }
+        //fill empty places with '0'
+        cout<<std::setfill('0')<<setw(8);
+        cout<<hex<<expandKEy[i]<<" ";
     }
-
-
-
     return 0;
 }
